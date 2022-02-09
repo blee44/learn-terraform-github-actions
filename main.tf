434b1af499ba51c3c741d2f08cc51f1c77b076b4
Brian Lee <7131624+blee44@users.noreply.github.com>
2022-02-08 17:26:03 -0700

    Revert "point backend to correct TFC organization and workspace"

--- main.tf
+++ main.tf
@@ -12,10 +12,10 @@ terraform {
   required_version = "~> 1.0"
 
   backend "remote" {
-    organization = "blee-acg-terraform-demos"
+    organization = "REPLACE_ME"
 
     workspaces {
-      name = "demo-github-actions"
+      name = "REPLACE_ME"
     }
   }
 }
